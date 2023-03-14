f1f503b90f395f228dc460ea1c658c919b945905
AliceLin321 <ataechiml24@gmail.com>
2023-03-14 13:51:07 -0400

    ubi
    ujoiu

---
+++ vue-proj/src/Home.vue
@@ -0,0 +1,22 @@
+
+<template>
+<div class ="home">
+<h1 v-if="graduated">{{ student }}</h1>
+<h1 v-else>"They did not yet graduate"</h1>
+</div>
+</template>
+
+<script>
+export default {
+ name: "Home",
+ components: {},
+data() {
+return {
+student: "Harry",
+graduated: true,
+};
+}}
+
+</script>
+<style>
+</style>
