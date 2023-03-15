3f8434ac2a4446e536748b9459e7c4bc99e49dde
AliceLin321 <ataechiml24@gmail.com>
2023-03-15 13:57:50 -0400

    54uy
    656y5

---
+++ vue-proj/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class ="home">
+  <h1 v-if="graduated">{{ student }}</h1>
+  <h1 v-else>"They did not yet graduate"</h1>
+  <ul>
+    <li v-for="animal in animals" :key="animal"> {{ animal }}
+
+    </li>
+  </ul>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+   name: "Home",
+   components: {},
+  data() {
+  return {
+  student: "Harry",
+  graduated: true,
+  animals: ["Pig", "Horse", "Donkey", "Cow", "Duck"  ]
+  };
+  }}
+  </script>
+  <style>
+  h1 {
+    color: red
+  }
+  </style>
