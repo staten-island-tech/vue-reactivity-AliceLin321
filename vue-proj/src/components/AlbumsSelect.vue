78953b20405486011fa6e60a39331c889418f72b
AliceLin321 <ataechiml24@gmail.com>
2023-03-23 13:58:22 -0400

    ery
    e5ty

--- vue-proj/src/components/AlbumsSelect.vue
+++ vue-proj/src/components/AlbumsSelect.vue
@@ -1,4 +1,101 @@
-<script>
+<script setup>
+const albums = [
+    {
+        name: "Star",
+        image: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/419cth-9mHL._AC_SX679_.jpg",
+    },
+    {
+        name: "Magic Arcadia",
+        image: "https://m.media-amazon.com/images/I/51p8vY-3QML._AC_SL1000_.jpg",
+    },
+    {
+        name: "Magic Sanctuary",
+        image: "https://m.media-amazon.com/images/I/51KbgZBXtbL._AC_SL1000_.jpg",
+    },
+    {
+        name: "Eternity Port",
+        image: "https://i5.walmartimages.com/asr/90b6f6cd-bfe4-4b09-9d31-5e92c5c155c7.4a86454f6b5747e879289f74fda1bfe9.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+    },
+    {
+        name: "Eternity Starboard",
+        image: "https://i5.walmartimages.com/asr/107bbada-6452-4230-aa7f-0c3bd9dd71b1.09215f123eda25c8d52831b8cd602907.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+    },
+    {
+        name: "Blue Hour R",
+        image: "https://www.kpopusaonline.com/wp-content/uploads/2020/09/203.1-500x468.png",
+    },
+    {
+        name: "Blue Hour AR",
+        image: "https://www.kpopusaonline.com/wp-content/uploads/2020/09/203.2.png",
+    },
+    {
+        name: "Blue Hour VR",
+        image: "https://www.kpopusaonline.com/wp-content/uploads/2020/09/203.3.png",
+    },
 
+    {
+        name: "Freeze World",
+        image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/freeze_1.png?v=1620266413",
+    },
+
+    {
+        name: "Freeze You",
+        image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/you_1.png?v=1620266432",
+    },
+    {
+        name: "Freeze Boy",
+        image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/boy_1.png?v=1620266398",
+    },
+    {
+        name: "Fight",
+        image: "https://cdn.shopify.com/s/files/1/2420/2037/products/TXTRepackageAlbumTheChaosChapterFightOrEscapeFight_1100x.png?v=1666650712",
+    },
+    {
+        name: "Escape",
+        image: "https://cdn.shopify.com/s/files/1/2420/2037/products/TXTRepackageAlbumTheChaosChapterFightOrEscapeEscape_1100x.png?v=1666650723",
+    },
+    {
+        name: "Fight Together",
+        image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/escapetogether.png?v=1628276466",
+    },
+    {
+        name: "Escape Together",
+        image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/fighttogether.png?v=1628276535",
+    },
+
+    {
+        name: "Thursday's Child Hate",
+        image: "",
+    },
+    {
+        name: "Thursday's Child End",
+        image: "",
+    },
+    {
+        name: "Thursday's Child Mess",
+        image: "",
+    },
+    {
+        name: "Thursday's Child Tear",
+        image: "",
+    },
+
+    {
+        name: "Temptation Daydream ",
+        image: "",
+    },
+    {
+        name: "Temptation Nightmare",
+        image: "",
+    },
+    {
+        name: "Temptation Farewell",
+        image: "",
+    },
+    {
+        name: "Temptation Lullaby",
+        image: "",
+    },
+]
 
 </script>
