946e5bce72ceed7d2c2a91a334a5a85cae20c345
AliceLin321 <ataechiml24@gmail.com>
2023-03-31 13:55:51 -0400

    y dyt
    oiuhiojh

--- vue-proj/src/components/AlbumsSelect.vue
+++ vue-proj/src/components/AlbumsSelect.vue
@@ -3,11 +3,7 @@ const albums = [
     {
         name: "Star",
         image: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/419cth-9mHL._AC_SX679_.jpg",
-    },
-    {
-        name: "Magic Arcadia",
-        image: "https://m.media-amazon.com/images/I/51p8vY-3QML._AC_SL1000_.jpg",
-    },
+    }, 
     {
         name: "Magic Sanctuary",
         image: "https://m.media-amazon.com/images/I/51KbgZBXtbL._AC_SL1000_.jpg",
@@ -16,64 +12,23 @@ const albums = [
         name: "Eternity Port",
         image: "https://i5.walmartimages.com/asr/90b6f6cd-bfe4-4b09-9d31-5e92c5c155c7.4a86454f6b5747e879289f74fda1bfe9.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
     },
-    {
-        name: "Eternity Starboard",
-        image: "https://i5.walmartimages.com/asr/107bbada-6452-4230-aa7f-0c3bd9dd71b1.09215f123eda25c8d52831b8cd602907.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
-    },
+    
     {
         name: "Blue Hour R",
         image: "https://www.kpopusaonline.com/wp-content/uploads/2020/09/203.1-500x468.png",
     },
     {
-        name: "Blue Hour AR",
-        image: "https://www.kpopusaonline.com/wp-content/uploads/2020/09/203.2.png",
-    },
-    {
-        name: "Blue Hour VR",
-        image: "https://www.kpopusaonline.com/wp-content/uploads/2020/09/203.3.png",
-    },
-
-    {
         name: "Freeze World",
         image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/freeze_1.png?v=1620266413",
-    },
-
-    {
-        name: "Freeze You",
-        image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/you_1.png?v=1620266432",
-    },
-    {
-        name: "Freeze Boy",
-        image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/boy_1.png?v=1620266398",
     },
     {
         name: "Fight",
         image: "https://cdn.shopify.com/s/files/1/2420/2037/products/TXTRepackageAlbumTheChaosChapterFightOrEscapeFight_1100x.png?v=1666650712",
     },
-    {
-        name: "Escape",
-        image: "https://cdn.shopify.com/s/files/1/2420/2037/products/TXTRepackageAlbumTheChaosChapterFightOrEscapeEscape_1100x.png?v=1666650723",
-    },
-    {
-        name: "Fight Together",
-        image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/escapetogether.png?v=1628276466",
-    },
-    {
-        name: "Escape Together",
-        image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/fighttogether.png?v=1628276535",
-    },
 
     {
         name: "Thursday's Child Hate",
         image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/TXT_minisode2_Thumbnail_HATE.png?v=1649775478",
-    },
-    {
-        name: "Thursday's Child End",
-        image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/TXT_minisode2_Thumbnail_END.png?v=1649775463",
-    },
-    {
-        name: "Thursday's Child Mess",
-        image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/TXT_minisode2_Thumbnail_MESS.png?v=1649775492",
     },
     {
         name: "Thursday's Child Tear",
@@ -83,21 +38,6 @@ const albums = [
     {
         name: "Temptation Daydream ",
         image: "https://cdn.shopify.com/s/files/1/0087/5898/4801/products/O1CN01mQDcMz274LXT4mjR1__238137743.png?v=1671200874",
-    },
-    {
-        name: "Temptation Nightmare",
-        image: "https://cdn.shopify.com/s/files/1/0087/5898/4801/products/O1CN01jB51XE274LXX3Yg5m__238137743_1024x1024.png?v=1671200877",
-    },
-    {
-        name: "Temptation Farewell",
-        image: "https://cdn.shopify.com/s/files/1/0087/5898/4801/products/O1CN01mQDcMz274LXT4mjR1__238137743.png?v=1671200874",
-    },
-    {
-        name: "Temptation Lullaby",
-        image: "https://cdn.shopify.com/s/files/1/0247/9495/5839/products/TXTLullaby1000.png?v=1672849750",
-    },
-],
-
-
-
+    }, 
+]
 </script>
--- vue-proj/src/views/Home.vue
+++ vue-proj/src/views/Home.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+ <template>
  <div class="home">
     <h1 v-if="graduated">{{ student }}</h1>
     <h1 v-else>"They did not yet graduate"</h1>
@@ -10,70 +10,28 @@
       </li>
     </ul> 
   
-      <button @click="authState" v-if="loggedIn">LogOut</button>
-      <button @click="authState" v-else>Login</button> 
-    
- 
-    <input type="text" placeholder="edit me" v-model="message" />
-    <p>{{ message }}</p>
- 
-   <div>
-    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
-    <label for="jack">Jack</label>
-    <input type="checkbox" id="john" value="John" v-model="checkedNames" />
-    <label for="john">John</label>
-    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
-    <label for="mike">Mike</label>
-    <br />
-    <span> Checked names: {{ checkedNames }}</span>
-  </div> 
-   <div>
-    <select v-model="selected">
-      <option>A</option>
-      <option>B</option>
-      <option>C</option>
-    </select>
-    <spar>{{ selected }}</spar>
-  </div>
-  <div class="home">
-    <Button>Button One</Button>
-    <Button>Button Two</Button>
-  </div>
+      
 </template>
 
 <script>
-import Button from "../components/Button.vue"; 
+
 export default {
   name: 'Home',
   components: {
-    Button,
   },
   data() {
     return {
        student: 'Harry',
       graduated: true,
       animals: ['Pig', 'Horse', 'Donkey', 'Cow', 'Duck'],
-      loggedIn: true,
-      message: '',
-      checkedNames: [], 
-      selected: "",
+     
     };
   },
-   methods: {
-  authState: function() {
-    if(this.loggedIn===false){
-      this.loggedIn = true;
-    }
-    else{
-      this.loggedIn = false;
-    }
-  }
 } 
-}
 </script>
 
 <style>
 h1 {
   color: red;
 }
-</style> -->
+</style> 
